48841c8e651601b5bbd7c6d577b36d388ae0a9c7
peterpeterparker <david.dalbusco@outlook.com>
2022-04-07 14:52:21 +0200

    feat: canister balance

--- providers/ic/src/providers/admin/admin.providers.ts
+++ providers/ic/src/providers/admin/admin.providers.ts
@@ -1,21 +1,54 @@
 import {Identity} from '@dfinity/agent';
-import {CanisterStatus, _SERVICE as ManagerActor} from '../../canisters/manager/manager.did';
-import {createManagerActor} from '../../utils/manager.utils';
+import {Principal} from '@dfinity/principal';
+import {_SERVICE as DataBucketActor} from '../../canisters/data/data.did';
+import {_SERVICE as StorageBucketActor} from '../../canisters/storage/storage.did';
+import {BucketActor, getDataBucket, getStorageBucket} from '../../utils/manager.utils';
 import {getIdentity} from '../auth/auth.providers';
 
-export interface CanistersStatus {
-  data: CanisterStatus;
-  storage: CanisterStatus;
+export interface CanisterBalance {
+  bucketId: Principal | undefined;
+  balance: bigint;
 }
 
-export const canistersStatus = async (): Promise<CanistersStatus> => {
+export interface CanistersBalance {
+  data: CanisterBalance;
+  storage: CanisterBalance;
+}
+
+export const canistersBalance = async (): Promise<CanistersBalance> => {
   const identity: Identity | undefined = getIdentity();
 
   if (!identity) {
     throw new Error('No internet identity to get the canisters status');
   }
 
-  const managerActor: ManagerActor = await createManagerActor({identity});
+  const [dataBucket, storageBucket]: [
+    BucketActor<DataBucketActor>,
+    BucketActor<StorageBucketActor>
+  ] = await Promise.all([
+    getDataBucket({
+      identity
+    }),
+    getStorageBucket({identity})
+  ]);
 
-  return managerActor.getCanistersStatus();
+  const {actor: dataActor, bucketId: dataBucketId}: BucketActor<DataBucketActor> = dataBucket;
+  const {actor: storageActor, bucketId: storageBucketId}: BucketActor<StorageBucketActor> =
+    storageBucket;
+
+  const [dataBalance, storageBalance]: [bigint, bigint] = await Promise.all([
+    dataActor.cyclesBalance(),
+    storageActor.cyclesBalance()
+  ]);
+
+  return {
+    data: {
+      bucketId: dataBucketId,
+      balance: dataBalance
+    },
+    storage: {
+      bucketId: storageBucketId,
+      balance: storageBalance
+    }
+  };
 };
