a70f395cde5df41c7e6822a4319380a9ff49a17d
David Dal Busco <david.dalbusco@outlook.com>
2022-04-15 10:21:08 +0200

    feat: publish detail cards in overview (#3)

--- providers/ic/src/utils/publish.overview.utils.ts
+++ providers/ic/src/utils/publish.overview.utils.ts
@@ -1,4 +1,4 @@
-import {log, PublishData} from '@deckdeckgo/editor';
+import {log, Meta, PublishData, toDate} from '@deckdeckgo/editor';
 import {_SERVICE as StorageBucketActor} from '../canisters/storage/storage.did';
 import {EnvStore} from '../stores/env.store';
 import {StorageUpload, updateTemplate} from './publish.utils';
@@ -7,11 +7,13 @@ import {upload} from './storage.utils';
 export const publishOverview = async ({
   dataId,
   storageUpload,
-  publishData
+  publishData,
+  meta
 }: {
   dataId: string;
   storageUpload: StorageUpload;
   publishData: PublishData;
+  meta: Meta | undefined;
 }): Promise<void> => {
   const template: string = await htmlTemplate();
 
@@ -24,7 +26,8 @@ export const publishOverview = async ({
     dataId,
     template: html,
     publishData,
-    storageUpload
+    storageUpload,
+    meta
   });
 
   const {actor} = storageUpload;
@@ -58,42 +61,76 @@ const updateList = async ({
   dataId,
   template,
   storageUpload,
-  publishData
+  publishData,
+  meta
 }: {
   dataId: string;
   template: string;
   storageUpload: StorageUpload;
   publishData: PublishData;
+  meta: Meta | undefined;
 }): Promise<string> => {
-  const {title} = publishData;
-  const {fullUrl} = storageUpload;
-
-  const li: string = `<li data-id="${dataId}"><a href="${fullUrl}">${title}</a></li>`;
-
   const source: string | undefined = await htmlSource(storageUpload);
 
+  const link: string = newLink({dataId, meta, publishData, storageUpload});
+
   if (!source) {
-    return template.replace(/<!-- DECKDECKGO_DATA -->/, `${li}`);
+    return template.replace(/<!-- DECKDECKGO_DATA -->/, `${link}`);
   }
 
-  const matches: RegExpMatchArray[] = [...source.matchAll(/<li\x20.*?data-id=".*?".*?li>/gm)];
+  const matches: RegExpMatchArray[] = [...source.matchAll(/<a\x20.*?data-id=".*?"[\s\S]*?a>/gm)];
 
   if (!matches || matches.length <= 0) {
-    return template.replace(/<!-- DECKDECKGO_DATA -->/, `${li}`);
+    return template.replace(/<!-- DECKDECKGO_DATA -->/, `${link}`);
   }
 
   const list: string[] = matches.map((match: RegExpMatchArray) => match[0]);
 
-  const index: number = list.indexOf(li);
+  const index: number = list.findIndex((a: string) => a.indexOf(dataId) > -1);
 
   if (index > -1) {
     return template.replace(
       /<!-- DECKDECKGO_DATA -->/,
-      list.map((entry: string, i: number) => (i === index ? li : entry)).join('')
+      list.map((entry: string, i: number) => (i === index ? link : entry)).join('')
     );
   }
 
-  return template.replace(/<!-- DECKDECKGO_DATA -->/, [li, ...list].join(''));
+  return template.replace(/<!-- DECKDECKGO_DATA -->/, [link, ...list].join(''));
+};
+
+const newLink = ({
+  dataId,
+  storageUpload,
+  publishData,
+  meta
+}: {
+  dataId: string;
+  storageUpload: StorageUpload;
+  publishData: PublishData;
+  meta: Meta | undefined;
+}): string => {
+  const {title} = publishData;
+  const {fullUrl} = storageUpload;
+
+  const detail: string =
+    meta?.description !== undefined && meta?.description !== null && meta?.description !== ''
+      ? `<p class="description">${meta.description}</p>`
+      : '';
+
+  const {format}: Intl.DateTimeFormat = new Intl.DateTimeFormat('en', {
+    year: 'numeric',
+    month: 'short',
+    day: '2-digit',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  });
+  const publishedAt: string = `<p>Published: ${format(
+    toDate(meta?.published_at) ?? new Date()
+  )}</p>`;
+  const editedAt: string = `<p>Edited: ${format(toDate(meta?.updated_at) ?? new Date())}</p>`;
+
+  return `<a data-id="${dataId}" href="${fullUrl}" rel="noopener noreferrer"><article><h3>${title}</h3>${detail}${publishedAt}${editedAt}</article></a>`;
 };
 
 const htmlSource = async ({bucketUrl}: StorageUpload): Promise<string | undefined> => {
