fe8869920be6f1e9f7f8f9d0f0ecd1c5c8b770b3
peterpeterparker <david.dalbusco@outlook.com>
2022-07-02 15:03:22 +0200

    feat: if a social image link is provided, then do not upload a social image card on publish and use provided link

--- providers/ic/src/utils/publish.social.utils.ts
+++ providers/ic/src/utils/publish.social.utils.ts
@@ -16,10 +16,18 @@ export const updateTemplateSocialImage = ({
 }): string => {
   const {social_image_name} = data;
 
+  if (hasSocialImageLink(data)) {
+    const {social_image_link} = data;
+    return html.replaceAll('{{DECKDECKGO_SOCIAL_IMAGE}}', social_image_link);
+  }
+
   const pathname: string = `/${socialImageFolder}/${social_image_name}.${socialImageExtension}`;
 
   return html.replaceAll('{{DECKDECKGO_SOCIAL_IMAGE}}', `${bucketUrl}${pathname}`);
 };
+
+// User used an image in the first or second paragraph and we gonna use it for the social card assuming assuming crawlers can use it too
+const hasSocialImageLink = ({social_image_link}: PublishData): boolean => social_image_link !== undefined
 
 export const uploadSocialImage = async ({
   storageUpload,
@@ -29,6 +37,10 @@ export const uploadSocialImage = async ({
   publishData: PublishData;
 }): Promise<void> => {
   const {social_image_name, social_image_value} = publishData;
+
+  if (hasSocialImageLink(publishData)) {
+    return;
+  }
 
   if (!social_image_value) {
     return;
