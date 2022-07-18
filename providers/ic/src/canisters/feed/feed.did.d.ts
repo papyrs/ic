fdd0ef2b834ed6e42af18b7358b44986326216f1
peterpeterparker <david.dalbusco@outlook.com>
2022-07-18 21:58:38 +0200

    build: update did file

--- providers/ic/src/canisters/feed/feed.did.d.ts
+++ providers/ic/src/canisters/feed/feed.did.d.ts
@@ -1,27 +1,27 @@
-import type { Principal } from '@dfinity/principal';
-import type { ActorMethod } from '@dfinity/agent';
+import type {ActorMethod} from '@dfinity/agent';
+import type {Principal} from '@dfinity/principal';
 
 export interface BlogPost {
-  'id' : string,
-  'status' : BlogPostStatus,
-  'updated_at' : Time,
-  'meta' : Array<number>,
-  'fullPath' : string,
-  'storageId' : Principal,
-  'created_at' : Time,
+  id: string;
+  status: BlogPostStatus__1;
+  updated_at: Time;
+  meta: Array<number>;
+  fullPath: string;
+  storageId: Principal;
+  created_at: Time;
 }
-export type BlogPostStatus = { 'open' : null } |
-  { 'accepted' : null } |
-  { 'declined' : null };
-export type BlogPostStatus__1 = { 'open' : null } |
-  { 'accepted' : null } |
-  { 'declined' : null };
+export type BlogPostStatus = {open: null} | {accepted: null} | {declined: null};
+export type BlogPostStatus__1 = {open: null} | {accepted: null} | {declined: null};
+export type BlogPostStatus__2 = {open: null} | {accepted: null} | {declined: null};
 export interface Feed {
-  'accept' : ActorMethod<[Principal, string], undefined>,
-  'decline' : ActorMethod<[Principal, string], undefined>,
-  'list' : ActorMethod<[[] | [FeedFilter]], Array<[string, BlogPost]>>,
-  'submit' : ActorMethod<[string, BlogPost], undefined>,
+  accept: ActorMethod<[Principal, string], undefined>;
+  decline: ActorMethod<[Principal, string], undefined>;
+  list: ActorMethod<[[] | [FeedFilter]], Array<[string, BlogPost]>>;
+  submit: ActorMethod<[string, BlogPost], undefined>;
+  updateStatus: ActorMethod<[Principal, Principal, string, BlogPostStatus], undefined>;
 }
-export interface FeedFilter { 'status' : [] | [BlogPostStatus__1] }
+export interface FeedFilter {
+  status: [] | [BlogPostStatus__2];
+}
 export type Time = bigint;
 export interface _SERVICE extends Feed {}
--- providers/ic/src/canisters/feed/feed.utils.did.js
+++ providers/ic/src/canisters/feed/feed.utils.did.js
@@ -1,35 +1,39 @@
-export const idlFactory = ({ IDL }) => {
-  const BlogPostStatus__1 = IDL.Variant({
-    'open' : IDL.Null,
-    'accepted' : IDL.Null,
-    'declined' : IDL.Null,
+export const idlFactory = ({IDL}) => {
+  const BlogPostStatus__2 = IDL.Variant({
+    open: IDL.Null,
+    accepted: IDL.Null,
+    declined: IDL.Null
   });
-  const FeedFilter = IDL.Record({ 'status' : IDL.Opt(BlogPostStatus__1) });
-  const BlogPostStatus = IDL.Variant({
-    'open' : IDL.Null,
-    'accepted' : IDL.Null,
-    'declined' : IDL.Null,
+  const FeedFilter = IDL.Record({status: IDL.Opt(BlogPostStatus__2)});
+  const BlogPostStatus__1 = IDL.Variant({
+    open: IDL.Null,
+    accepted: IDL.Null,
+    declined: IDL.Null
   });
   const Time = IDL.Int;
   const BlogPost = IDL.Record({
-    'id' : IDL.Text,
-    'status' : BlogPostStatus,
-    'updated_at' : Time,
-    'meta' : IDL.Vec(IDL.Nat8),
-    'fullPath' : IDL.Text,
-    'storageId' : IDL.Principal,
-    'created_at' : Time,
+    id: IDL.Text,
+    status: BlogPostStatus__1,
+    updated_at: Time,
+    meta: IDL.Vec(IDL.Nat8),
+    fullPath: IDL.Text,
+    storageId: IDL.Principal,
+    created_at: Time
+  });
+  const BlogPostStatus = IDL.Variant({
+    open: IDL.Null,
+    accepted: IDL.Null,
+    declined: IDL.Null
   });
   const Feed = IDL.Service({
-    'accept' : IDL.Func([IDL.Principal, IDL.Text], [], []),
-    'decline' : IDL.Func([IDL.Principal, IDL.Text], [], []),
-    'list' : IDL.Func(
-        [IDL.Opt(FeedFilter)],
-        [IDL.Vec(IDL.Tuple(IDL.Text, BlogPost))],
-        ['query'],
-      ),
-    'submit' : IDL.Func([IDL.Text, BlogPost], [], []),
+    accept: IDL.Func([IDL.Principal, IDL.Text], [], []),
+    decline: IDL.Func([IDL.Principal, IDL.Text], [], []),
+    list: IDL.Func([IDL.Opt(FeedFilter)], [IDL.Vec(IDL.Tuple(IDL.Text, BlogPost))], ['query']),
+    submit: IDL.Func([IDL.Text, BlogPost], [], []),
+    updateStatus: IDL.Func([IDL.Principal, IDL.Principal, IDL.Text, BlogPostStatus], [], ['oneway'])
   });
   return Feed;
 };
-export const init = ({ IDL }) => { return [IDL.Text]; };
+export const init = ({IDL}) => {
+  return [IDL.Text];
+};
