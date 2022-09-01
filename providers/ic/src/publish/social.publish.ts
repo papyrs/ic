243cd93c7e1ba156247a63fff27909d1bdf56fe3
peterpeterparker <david.dalbusco@outlook.com>
2022-09-01 08:06:48 +0200

    fix: social image card in case of images as second paragraph

--- providers/ic/src/publish/social.publish.ts
+++ providers/ic/src/publish/social.publish.ts
@@ -16,7 +16,7 @@ export const updateTemplateSocialImage = ({
 }): string => {
   const {social_image_name} = data;
 
-  if (hasSocialImageLink(data)) {
+  if (hasUnsplashSocialImageLink(data)) {
     const {social_image_link} = data;
     return html.replaceAll('{{DECKDECKGO_SOCIAL_IMAGE}}', social_image_link);
   }
@@ -26,9 +26,13 @@ export const updateTemplateSocialImage = ({
   return html.replaceAll('{{DECKDECKGO_SOCIAL_IMAGE}}', `${bucketUrl}${pathname}`);
 };
 
-// User used an image in the first or second paragraph and we gonna use it for the social card assuming assuming crawlers can use it too
-const hasSocialImageLink = ({social_image_link}: PublishData): boolean =>
-  social_image_link !== undefined;
+// Did user used an image from an Unsplash in the first or second paragraph? If yes, we gonna use it for the social card.
+//
+// Note: We don't use users' images because we are blocking all crawlers to crawl /images/ in robots.txt to keep the images private.
+// We would be able to add "allow" exceptions in the robots.txt (assuming it is supported by twitter bots) but it would mean that we would have to maintain a list of images that are public (e.g. what if user delete and image?).
+// That's why, at least for now and for simplicity reason, we don't use users' own images as social images but generate the cards.
+const hasUnsplashSocialImageLink = ({social_image_link}: PublishData): boolean =>
+  social_image_link !== undefined && social_image_link.includes('unsplash.com');
 
 export const uploadSocialImage = async ({
   storageUpload,
@@ -39,7 +43,7 @@ export const uploadSocialImage = async ({
 }): Promise<void> => {
   const {social_image_name, social_image_value} = publishData;
 
-  if (hasSocialImageLink(publishData)) {
+  if (hasUnsplashSocialImageLink(publishData)) {
     return;
   }
 
