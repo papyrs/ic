3a615ac2ccb770c217e9cb113239c2df1083bc52
David Dal Busco <david.dalbusco@outlook.com>
2022-09-18 17:06:49 +0200

    fix: upload /index.html first

--- providers/ic/src/publish/metas.publish.ts
+++ providers/ic/src/publish/metas.publish.ts
@@ -80,8 +80,10 @@ const publishMetas = async ({
 }): Promise<void> => {
   const metas: PublishMeta[] = sortPublishMetaEntries(entries);
 
+  // Do not upload all metas at the same time to not stress the storage canisters
+  await publishIndexHtml({storageUpload, publishData, metas});
+
   const promises: Promise<void>[] = [
-    publishIndexHtml({storageUpload, publishData, metas}),
     publishSitemap({storageUpload, metas}),
     publishRSS({storageUpload, metas, publishData})
   ];
