7a038916de254b3ad38169e4d462cd674e25ca86
David Dal Busco <david.dalbusco@outlook.com>
2022-09-30 12:42:57 +0200

    feat: generate did for interactions

--- providers/ic/src/canisters/data/data.utils.did.js
+++ providers/ic/src/canisters/data/data.utils.did.js
@@ -2,13 +2,21 @@ export const idlFactory = ({IDL}) => {
   const UserId = IDL.Principal;
   const Time = IDL.Int;
   const DelData = IDL.Record({id: IDL.Text, updated_at: Time});
+  const DelInteraction = IDL.Record({id: IDL.Text, updated_at: Time});
   const Data = IDL.Record({
     id: IDL.Text,
     updated_at: Time,
     data: IDL.Vec(IDL.Nat8),
     created_at: Time
   });
-  const DataFilter = IDL.Record({
+  const Interaction = IDL.Record({
+    id: IDL.Text,
+    updated_at: Time,
+    data: IDL.Vec(IDL.Nat8),
+    created_at: Time,
+    author: UserId
+  });
+  const RecordFilter = IDL.Record({
     notContains: IDL.Opt(IDL.Text),
     startsWith: IDL.Opt(IDL.Text)
   });
@@ -18,13 +26,26 @@ export const idlFactory = ({IDL}) => {
     data: IDL.Vec(IDL.Nat8),
     created_at: IDL.Opt(Time)
   });
+  const PutInteraction = IDL.Record({
+    id: IDL.Text,
+    updated_at: IDL.Opt(Time),
+    data: IDL.Vec(IDL.Nat8),
+    created_at: IDL.Opt(Time),
+    author: UserId
+  });
   const DataBucket = IDL.Service({
+    countLikes: IDL.Func([IDL.Text], [IDL.Nat], []),
     cyclesBalance: IDL.Func([], [IDL.Nat], ['query']),
     del: IDL.Func([IDL.Text], [], []),
     delete: IDL.Func([IDL.Text, DelData], [], []),
+    deleteInteraction: IDL.Func([IDL.Text, DelInteraction], [], []),
     get: IDL.Func([IDL.Text], [IDL.Opt(Data)], ['query']),
-    list: IDL.Func([IDL.Opt(DataFilter)], [IDL.Vec(IDL.Tuple(IDL.Text, Data))], ['query']),
+    getComment: IDL.Func([IDL.Text, IDL.Text], [IDL.Opt(Interaction)], ['query']),
+    getLike: IDL.Func([IDL.Text], [IDL.Opt(Interaction)], ['query']),
+    list: IDL.Func([IDL.Opt(RecordFilter)], [IDL.Vec(IDL.Tuple(IDL.Text, Data))], ['query']),
+    listComments: IDL.Func([IDL.Text], [IDL.Vec(IDL.Tuple(IDL.Text, Interaction))], []),
     put: IDL.Func([IDL.Text, PutData], [Data], []),
+    putInteraction: IDL.Func([IDL.Text, PutInteraction], [Interaction], []),
     set: IDL.Func([IDL.Text, Data], [], []),
     transferFreezingThresholdCycles: IDL.Func([], [], [])
   });
