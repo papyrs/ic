515e5c20cc7c1ef060f539954345053835c75f9e
David Dal Busco <david.dalbusco@outlook.com>
2022-10-30 13:59:36 +0100

    feat: inline sha256 calculation for motoko migration

--- canisters/src/storage/src/impls_mo.rs
+++ canisters/src/storage/src/impls_mo.rs
@@ -5,7 +5,9 @@
 // TODO: to be deleted
 //
 
+use ic_cdk::{api::{time}};
 use std::collections::HashMap;
+use sha2::{Digest, Sha256};
 use crate::constants::ASSET_ENCODING_KEY_RAW;
 use crate::types::store::{Asset, AssetEncoding, AssetKey};
 use crate::types_mo::mo::store::MoAsset;
@@ -14,7 +16,19 @@ impl From<&MoAsset> for Asset {
     fn from(MoAsset {key: mo_key, headers: mo_headers, encoding: mo_encoding}: &MoAsset) -> Self {
         let mut encodings = HashMap::new();
 
-        encodings.insert(ASSET_ENCODING_KEY_RAW.to_string(), AssetEncoding::from(&mo_encoding.contentChunks));
+        // Calculate sha256 - Duplicate implementation to preserve easily the original Motoko "modified" and "total_length" information
+        let mut hasher = Sha256::new();
+        for chunk in mo_encoding.contentChunks.iter() {
+            hasher.update(chunk);
+        }
+        let sha256 = hasher.finalize().into();
+
+        encodings.insert(ASSET_ENCODING_KEY_RAW.to_string(), AssetEncoding {
+            modified: u64::try_from(mo_encoding.modified.clone().0).unwrap_or(time()),
+            content_chunks: mo_encoding.contentChunks.clone(),
+            total_length: mo_encoding.totalLength,
+            sha256
+        });
 
         Asset {
            key: AssetKey {
