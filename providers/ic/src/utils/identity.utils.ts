c3dcfd57a417776a797a934d893cdcfb62ee5362
David Dal Busco <david.dalbusco@outlook.com>
2023-03-11 16:49:12 +0100

    fix: hotfix workaround for agent-js

--- providers/ic/src/utils/identity.utils.ts
+++ providers/ic/src/utils/identity.utils.ts
@@ -1,6 +1,6 @@
 import {Identity} from '@dfinity/agent';
 import {KEY_STORAGE_DELEGATION, KEY_STORAGE_KEY} from '@dfinity/auth-client';
-import {DelegationChain, DelegationIdentity, ECDSAKeyIdentity} from '@dfinity/identity';
+import {DelegationChain, DelegationIdentity, ECDSAKeyIdentity, Ed25519KeyIdentity} from '@dfinity/identity';
 import {createStore, getMany} from 'idb-keyval';
 
 export const internetIdentityAuth = async (): Promise<
@@ -20,10 +20,19 @@ export const initIdentity = async ({
   identityKey,
   delegationChain
 }: {
-  identityKey: CryptoKeyPair | null;
+  identityKey: CryptoKeyPair | string | null;
   delegationChain: string | null;
 }): Promise<Identity> => {
   const chain: DelegationChain = DelegationChain.fromJSON(delegationChain);
+
+  // TODO: remove me in a bit (11.03.2023)
+  // Turns out agent-js fails at migrating existing key in production
+  if (typeof identityKey === "string") {
+    const key: Ed25519KeyIdentity = Ed25519KeyIdentity.fromJSON(identityKey);
+
+    return DelegationIdentity.fromDelegation(key, chain);
+  }
+
   const key: ECDSAKeyIdentity = await ECDSAKeyIdentity.fromKeyPair(identityKey);
 
   return DelegationIdentity.fromDelegation(key, chain);
